dd85f6b3b1007c445f73f201a3289ad87f0054e3
odpay <odpay@odpay.net>
2026-01-21 20:36:42 +1100

    plugins copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,13 @@ if [ -d /defaults ] && [ "$(ls -A /defaults)" ]; then
     cp -a /defaults/. /opt/ZenithProxy/
 fi
 
+# If /plugins exists and is not empty, copy everything to /opt/ZenithProxy/plugins
+if [ -d /plugins ] && [ "$(ls -A /plugins)" ]; then
+    echo "Copying files from /defaults to /opt/ZenithProxy/plugins..."
+    mkdir /opt/ZenithProxy/plugins/
+    cp -a /plugins/. /opt/ZenithProxy/plugins/
+fi
+
 cd /opt/ZenithProxy
 chmod +x ./launch
 exec ./launch --unattended
